8443dbe477491d92936c2b4ae01ec34ce64fbcc3
Allen Cho <choaj96s@gmail.com>
2026-01-19 19:02:39 -0800

    fix the bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "nba-infra-terraform-state"
+    bucket         = "nba-infra-terraform-state-ac"
     key            = "nba-infra/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "nba-infra-terraform-lock"
